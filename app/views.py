64e1c8cd79ff024b57a4ed44f9b8980996a9d1aa
IanoSang <ianosang18@gmail.com>
2022-05-15 11:44:39 +0300

    feat: views- adds a routing to  the account page and account logout

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
